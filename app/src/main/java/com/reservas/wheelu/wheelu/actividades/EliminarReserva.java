17174fdd4e7ab8af86ee2af17bbc6f14501956a5
Dumaru <robinsonmu@unisabana.edu.co>
2018-11-21 13:22:02 -0500

    Activities RM

---
+++ app/src/main/java/com/reservas/wheelu/wheelu/actividades/EliminarReserva.java
@@ -0,0 +1,31 @@
+package com.reservas.wheelu.wheelu.actividades;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.reservas.wheelu.wheelu.R;
+
+public class EliminarReserva extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eliminar_reserva);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
