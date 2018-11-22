7c023fa4398bc9e49429de06fc6d6c0bf3b9875e
Dumaru <robinsonmu@unisabana.edu.co>
2018-11-22 02:20:15 -0500

    Test Eliminar

---
+++ app/src/test/java/com/reservas/wheelu/wheelu/ReservasIntegracion.java
@@ -0,0 +1,10 @@
+package com.reservas.wheelu.wheelu;
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class ReservasIntegracion {
+    @Test
+    public void crear_cons_act_eliminar(){
+
+    }
+
+}
